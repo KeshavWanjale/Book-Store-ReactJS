2291cdec3d17b92d7c49420a42fd3b62125a1a09
unknown <keshav.wanjale@gmail.com>
2024-12-27 15:37:42 +0530

    [Keshav] ADD: did redux setup

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,9 +1,14 @@
 import RoutingMoudule from "./RoutingMoudule";
+import store from "./redux/store";
+import { Provider } from 'react-redux'
 
 function App() {
   return (
     <div>
-      <RoutingMoudule />
+      <Provider store={store}>
+        <RoutingMoudule />
+      </Provider>
+
     </div>
   );
 }
